bb3e1568477e6ca375f0aee2567adf04086e5555
zhihui123.zhang <zhihui123.zhang@huolala.cn>
2023-12-22 16:02:38 +0800

    feat: test how to use components

--- src/routes/Counter.svelte
+++ src/routes/Counter.svelte
@@ -1,5 +1,6 @@
 <script lang="ts">
 	import { spring } from 'svelte/motion';
+	import { Button } from "$lib/components/ui/button";
 
 	let count = 0;
 
@@ -14,6 +15,7 @@
 </script>
 
 <div class="counter">
+	<Button variant="outline">Button</Button>
 	<button on:click={() => (count -= 1)} aria-label="Decrease the counter by one">
 		<svg aria-hidden="true" viewBox="0 0 1 1">
 			<path d="M0,0.5 L1,0.5" />
