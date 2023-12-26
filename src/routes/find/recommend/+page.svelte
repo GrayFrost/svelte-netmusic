dd38fcb82a8574bd6c0ab130f0bc8915327a0e65
zhihui123.zhang <zhihui123.zhang@huolala.cn>
2023-12-26 11:33:22 +0800

    feat: 推荐页布局

--- src/routes/find/recommend/+page.svelte
+++ src/routes/find/recommend/+page.svelte
@@ -1,8 +1,72 @@
 <script lang="ts">
   import Slider from '$components/slider.svelte';
+
+  let recommends = [1,2,3,4,5, 6, 7,8,9, 10];
+  let plays = [1,2,3,4];
+  let latests = [1,2,3,4,5,6,7,8,9,10];
+  let mvs = [1,2];
+  let podcasts = [1,2,3,4,5,6];
+  let lives = [1,2,3,4];
 </script>
 
 <div>
   <h2>推荐</h2>
   <Slider />
+  <section>
+    <h3>推荐歌单</h3>
+    <div class="grid grid-cols-5 gap-4">
+      {#each recommends as recommendItem}
+        <div class="bg-slate-400 rounded">
+          <div>图片</div>
+          <span>标题</span>
+        </div>
+      {/each}
+    </div>
+  </section>
+  <section>
+    <h3>独家放送</h3>
+    <div class="grid grid-cols-4 gap-4">
+      {#each plays as playItem, index}
+        <div class="bg-slate-500 rounded">
+          独家放送{index}
+        </div>
+      {/each}
+    </div>
+  </section>
+  <section>
+    <h3>最新音乐</h3>
+    <div class="grid grid-cols-2 gap-4">
+      {#each latests as latestItem, index}
+        <div class="bg-slate-700 rounded">
+          最新音乐{index}
+        </div>
+      {/each}
+    </div>
+  </section>
+  <section>
+    <h3>推荐MV</h3>
+    <div class="flex">
+      {#each mvs as mvItem, index}
+        <div class="mr-4 rounded bg-slate-800">mv{index}</div>
+      {/each}
+    </div>
+  </section>
+  <section>
+    <h3>播客</h3>
+    <div class="grid grid-cols-2 gap-4">
+      {#each podcasts as podcastItem, index}
+        <div class="bg-red-400 rounded">播客{index}</div>
+      {/each}
+    </div>
+  </section>
+  <section>
+    <h3>Look直播</h3>
+    <div class="grid grid-cols-4 gap-4">
+      {#each lives as liveItem, index}
+        <div class="bg-red-500 rounded">
+          直播{index}
+        </div>
+      {/each}
+    </div>
+  </section>
 </div>
